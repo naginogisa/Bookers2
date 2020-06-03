6bdd2eac0b6a83c614eafb91dd7c8c4241a033c6
nozakifamima <saginogi@gmail.com>
2020-06-03 15:54:13 +0900

    [finish] 3-9

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'b50b7a163d4c988d15b2e3202afefe7fdd82908dd97166746ff9477c194cc70cc4eeca24e6f81a74bde30501e2a34ad4d64e5eddb508c3cb3145d2e55e20251a'
